cefd8ef94c109b3501453c31795ca13358e10074
jeromydean <jeromydean@gmail.com>
2025-10-14 11:10:56 -0400

    Added initial version of UI

---
+++ COMWizard/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace COMWizard.ViewModels
+{
+  public class ViewModelBase : ObservableObject
+  {
+  }
+}
---
+++ COMWizard/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace COMWizard.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
